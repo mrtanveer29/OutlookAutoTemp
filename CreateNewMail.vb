3644e9d3e12829dde1635ac9b52501330d66d3a0
Tanveer Hasan <mrtanveer.29@gmail.com>
2021-03-09 09:56:57 +0600

    Update CreateNewMail.vb

--- CreateNewMail.vb
+++ CreateNewMail.vb
@@ -8,42 +8,124 @@ Sub CreateNewMail()
     Dim timediff As Integer
      Dim phour As Integer
       Dim pmin As Integer
-       Dim currTime As String
+       Dim currTime() As String
+       Dim exitTime As String
        Dim signature As String
+    Dim entry As String
+    Dim break As String
+      Dim breakhour As Integer
+      Dim breakmin As Integer
+    Dim LArray() As String
     
- 
+    entry = InputBox("Give me Entry time")
+    break = InputBox("Break time in minute")
+    LArray = Split(entry, ":")
+    
+    entryminute = LArray(0) * 60 + LArray(1)
+    
     Set obApp = Outlook.Application
     Set NewMail = obApp.CreateItem(0)
     ' Set Body = Application.CreateItemFromTemplate("C:\Users\Mahmuda Hasan\AppData\Roaming\Microsoft\Templates\Work log.oft")
-    currTime = Format(Now(), "HH:mm")
+    exitTime = InputBox("Give me Exit time")
+    currTime = Split(exitTime, ":")
   
-    endhour = Hour(currTime)
-    endminute = Minute(currTime)
+  breakhour = Int((break / 60))
+    breakmin = break - (breakhour * 60)
+    
+    endhour = currTime(0) + 12
+    endminute = currTime(1)
    
-    timediff = (endhour * 60 + endminute) - 510
-    phour = timediff / 60
-    pmin = timediff Mod 60
+    timediff = (endhour * 60 + endminute) - (entryminute + break)
+    phour = Int(timediff / 60)
+    pmin = timediff - (phour * 60)
     
     
     
     style = "<style>table{font-family:arial,sans-serif;border-collapse:collapse;width:100%;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}tr:nth-child(even){background-color:#dddddd;}</style>"
-    HTMLBody = style & "<table>"
+     
+     HTMLBody = "    <h3 style='color:#DAA520'>Tanveer Hasan's Work Log for " & Format(Now(), "dd/MM/yyyy") & ":</h3> "
+     
+      HTMLBody = HTMLBody & style & "<table>"
+    HTMLBody = HTMLBody & "   <tbody>  "
+    HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> Task</td>  "
+     HTMLBody = HTMLBody & "       <td>Description</td>  "
+      HTMLBody = HTMLBody & "       <td>Time</td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+    HTMLBody = HTMLBody & "     <tr>  "
+
+    HTMLBody = HTMLBody & "       <td> Meeting</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> Estimation</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> R&D</td>  "
+     HTMLBody = HTMLBody & "       <td> <table><tr><td style='width:80%'>.</td><td></td></tr></table></td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> Requirement Analysis</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> Code Review</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     <tr>  "
+   
+    HTMLBody = HTMLBody & "       <td> Issue</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+            HTMLBody = HTMLBody & "     <tr>  "
+
+    HTMLBody = HTMLBody & "       <td> Feature</td>  "
+     HTMLBody = HTMLBody & "       <td> </td>  "
+      HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+    HTMLBody = HTMLBody & "     </tbody>  "
+    HTMLBody = HTMLBody & "     </table> "
+        HTMLBody = HTMLBody & "   <h3 style='color:#DAA520'>Planning</h3>  "
+     HTMLBody = HTMLBody & style & "<table>"
+    HTMLBody = HTMLBody & "   <tbody>  "
+    HTMLBody = HTMLBody & "     <tr>  "
+    HTMLBody = HTMLBody & "       <td>Target</td>  "
+    HTMLBody = HTMLBody & "       <td> Due on</td>  "
+     HTMLBody = HTMLBody & "       <td>Status</td>  "
+    HTMLBody = HTMLBody & "     </tr>  "
+        HTMLBody = HTMLBody & "     </tbody>  "
+    HTMLBody = HTMLBody & "     </table> "
+    
+    HTMLBody = HTMLBody & "   <h3 style='color:#DAA520'>Summary</h3>  "
+    HTMLBody = HTMLBody & style & "<table>"
     HTMLBody = HTMLBody & "   <tbody>  "
     HTMLBody = HTMLBody & "     <tr>  "
     HTMLBody = HTMLBody & "       <td>Start Time</td>  "
-    HTMLBody = HTMLBody & "       <td>8:30 AM</td>  "
+    HTMLBody = HTMLBody & "       <td>" & entry & " AM</td>  "
     HTMLBody = HTMLBody & "     </tr>  "
     HTMLBody = HTMLBody & "     <tr>  "
     HTMLBody = HTMLBody & "       <td>End Time</td>  "
-    HTMLBody = HTMLBody & "       <td>" & Format(Now(), "HH:mm AM/PM") & "</td>  "
+    HTMLBody = HTMLBody & "       <td>" & exitTime & " PM</td>  "
     HTMLBody = HTMLBody & "     </tr>  "
     HTMLBody = HTMLBody & "     <tr>  "
     HTMLBody = HTMLBody & "       <td>Various Breaks</td>  "
-    HTMLBody = HTMLBody & "       <td></td>  "
+    HTMLBody = HTMLBody & "       <td>" & breakhour & " hour(s) " & breakmin & " minute(s) </td>  "
     HTMLBody = HTMLBody & "     </tr>  "
     HTMLBody = HTMLBody & "     <tr>  "
     HTMLBody = HTMLBody & "       <td>Productive Working Hours</td>  "
-    HTMLBody = HTMLBody & "       <td>" & phour & ":" & pmin & " h </td>  "
+    HTMLBody = HTMLBody & "       <td>" & phour & " hour(s) " & pmin & " minute(s)  </td>  "
     HTMLBody = HTMLBody & "     </tr>  "
     HTMLBody = HTMLBody & "     <tr>  "
     HTMLBody = HTMLBody & "  <td>Internet Condition for Today</td>  "
@@ -66,20 +148,19 @@ Sub CreateNewMail()
     HTMLBody = HTMLBody & "       <td></td>  "
     HTMLBody = HTMLBody & "     </tr>  "
     HTMLBody = HTMLBody & "     </tbody>  "
-    HTMLBody = HTMLBody & "     </table>  <br/> "
-     HTMLBody = HTMLBody & "    <h4>Work Log:</h4> "
-     HTMLBody = HTMLBody & "    <ol><li></li></ol> "
+    HTMLBody = HTMLBody & "     </table> "
     'You can change the concrete info as per your needs
     With NewMail
         .Display
     End With
 
+
 signature = NewMail.HTMLBody
     With NewMail
-         .Subject = "Work From Home - [Name: Tanveer Hasan]  - " & Date & " (Work - Full) "
-         .To = "workfromhome@bitmascot.com"
-         .CC = "ghosh@bitmascot.com"
-         .BCC = "tasnia@webalive.com.au"
+         .Subject = "Work From Home - [HPDS2 (Chamera & BP)] -[Name: Tanveer Hasan]  - " & Format(Now(), "dd/MM/yyyy") & " (Work - Full) "
+         .To = "anick@bitmascot.com;"
+         .CC = "hpds2-team@bitmascot.com;liakat@bitmascot.com"
+         .BCC = ""
          .HTMLBody = HTMLBody & signature
          
          .Importance = olImportanceNormal
